8c96321afd6bf8717b5b6c5c95d771ea2fc252a8
bergs643@umn.edu <bergs643@umn.edu>
2023-10-25 17:30:52 -0500

    some work in root_process, not done

--- src/root_process.c
+++ src/root_process.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "../include/utils.h"
+#include "../include/sha256.h"
 
 #define WRITE (O_WRONLY | O_CREAT | O_TRUNC)
 #define PERM (S_IRUSR | S_IWUSR)
@@ -13,12 +14,12 @@ char *output_file_folder = "output/final_submission/";
 void redirection(char **dup_list, int size, char* root_dir){
     // TODO(overview): redirect standard output to an output file in output_file_folder("output/final_submission/")
     // TODO(step1): determine the filename based on root_dir. e.g. if root_dir is "./root_directories/root1", the output file's name should be "root1.txt"
-    char *root_dir = extract_root_directory(root_dir);
-    char root[strlen(root_dir) - 1]; //getting rid of the slash at the end of root dir
-    sprintf(root, "%s", root_dir);
+    char *root = extract_root_directory(root_dir); // should make fname = "root1" in above example
+    // char root[strlen(root_dir) - 1]; //getting rid of the slash at the end of root dir
+    // sprintf(root, "%s", root_dir);
 
     char file_name[strlen(root) + 10];
-    sprintf(file_name, "%s.txt", root);
+    sprintf(file_name, "%s.txt", root); // file_name should now = root1.txt
     //TODO(step2): redirect standard output to output file (output/final_submission/root*.txt)
     int TEMP_STDOUT_FILENO = dup(STDOUT_FILENO);
     int fp = open(file_name, WRITE, PERM);
@@ -42,20 +43,43 @@ void redirection(char **dup_list, int size, char* root_dir){
     close(fp);
     //TODO(step3): read the content each symbolic link in dup_list, write the path as well as the content of symbolic link to output file(as shown in expected)
     for (int i=0; i < size; i++) {
-        
+        char buf[PATH_MAX];
+
+        int links = open(dup_list[i], O_RDONLY);
+        if (links == -1) {
+            perror("Failed to open symbolic link file\n");
+            exit(-1);
+        }
+
+        int retained_path = readlink(dup_list[i], buf, sizeof(buf)); // prof suggested to use a linux func to get path on piazza, not sure if readlink is right
+        if (retained_path == -1) {
+            perror("Failed to read symbolic link\n");
+            exit(-1);
+        }
+
+        fprintf(stdout, "[<path of symbolic link> --> <path of retained file>] : %s --> %s]\n", dup_list[i], buf); 
     }
 }
 
 void create_symlinks(char **dup_list, char **retain_list, int size) {
     //TODO(): create symbolic link at the location of deleted duplicate file
-
     //TODO(): dup_list[i] will be the symbolic link for retain_list[i]
-
+    for (int i = 0; i < size; i++) {
+            if (symlink(retain_list[i], dup_list[i]) == -1) {
+                perror("Failed to create a symbolic link\n");
+                exit(-1);
+            }
+        }
 }
 
 void delete_duplicate_files(char **dup_list, int size) {
     //TODO(): delete duplicate files, each element in dup_list is the path of the duplicate file
-
+    for (int i = 0; i < size; i++) {
+        if (remove(dup_list[i]) == -1) {
+            perror("Failed to delete duplicate file\n");
+            exit(-1);
+        }
+    }
 }
 
 // ./root_directories <directory>
@@ -89,5 +113,6 @@ int main(int argc, char* argv[]) {
     redirection(dup_list, size, argv[1]);
 
     //TODO(step5): free any arrays that are allocated using malloc!!
-
+    free(dup_list);
+    free(retain_list);
 }

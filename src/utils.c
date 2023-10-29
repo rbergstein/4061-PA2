f59adbf81e9257f35590495eee1d67361e7b74b3
mawarfa <146888786+mawarfa@users.noreply.github.com>
2023-10-28 22:15:38 -0500

    update utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,19 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "../include/utils.h"
+
+
+void perfectFormat(char *buffer, const char *find, const char *replace) {
+    char *occurrence = buffer;
+    int find_len = strlen(find);
+    int replace_len = strlen(replace);
+
+    while ((occurrence = strstr(occurrence, find))) {
+        memmove(occurrence + replace_len, occurrence + find_len, strlen(occurrence + find_len) + 1);
+        memcpy(occurrence, replace, replace_len);
+        occurrence += replace_len;
+    }
+}
 
 // Extracts the filename from the full path
 // e.g. path="./root_directories/root1/sub_1/WorldStage_2.txt"
@@ -12,6 +24,7 @@ char* extract_filename(char* path) {
         return path;
     return filename + 1;
 }
+
 
 // Extracts the directory name after "root_directories"
 // e.g. path="./root_directories/original_root1/sub_1/WorldStage_2.txt"
@@ -28,7 +41,7 @@ char* extract_root_directory(const char* path) {
     if (end_of_root == NULL){
         return strdup(start_of_root);  // In case the path ends with the root directory
     }
-    int length = end_of_root - start_of_root + 1; // +1 to include the trailing slash
+    int length = end_of_root - start_of_root;// + 1; // +1 to include the trailing slash
     char* result = (char*)malloc(length + 1);  // +1 for the null terminator
     strncpy(result, start_of_root, length);
     result[length] = '\0';  // Null-terminate the result string
@@ -56,19 +69,24 @@ void remove_filepath_duplicate(char **dup_list, char **retain_list, int *size) {
     }
 }
 
-
 void sanitize_dup_retain(char **dup_list, char **retain_list, int size) {
     for (int m = 0; m < size; m++) {
         for (int n = 0; n < size; n++) {
-            if (strcmp(retain_list[m], dup_list[n]) == 0) {
+            if (retain_list[m]!=NULL && dup_list[n]!=NULL && strcmp(retain_list[m], dup_list[n]) == 0) {
                 if (retain_list[m] != retain_list[n]) {
-                    free(retain_list[m]);
-                    retain_list[m] = dup_list[n];
+                    if(retain_list[m]) free(retain_list[m]);
+                    retain_list[m] = retain_list[n];
                 }
                 break;
             }
         }
     }
+}
+
+int get_file_num(char* filename) {
+    int length = strlen(filename);
+    int num = atoi(&filename[length - 5]);
+    return num;
 }
 
 int parse_hash(char * file_hashes, char** dup_list, char** retain_list) {
@@ -86,46 +104,59 @@ int parse_hash(char * file_hashes, char** dup_list, char** retain_list) {
     }
 
     int index = 0;
-
+        
     for(int i = 1; i < count; i += 2) {
-        int retain_index = i; // index of file to be retained
-
-        // Look for other files with the same hash
+        int retain_index = i;
         for (int j = i + 2; j < count; j += 2) {
-            if (strcmp(array[i], array[j]) == 0) {
-                
-                if (retain_index == i) { // if first match, add i-1 to retain_list
-                    retain_list[index] = malloc(sizeof(char) * 128);
+            if (strcmp(array[i], "") != 0 && strcmp(array[i], array[j]) == 0) {
+                int numA = get_file_num(array[retain_index - 1]);
+                int numB = get_file_num(array[j - 1]);
+
+                if (numB < numA) {
+                    // This condition ensures that the retain_list entry is set every time a duplicate is found
                     if (!retain_list[index]) {
+                        retain_list[index] = malloc(sizeof(char) * 128);
+                        if (!retain_list[index]) {
+                            fprintf(stderr, "Memory allocation failed!\n");
+                            exit(1);
+                        }
+                        strcpy(retain_list[index], array[j - 1]);
+                    }
+
+                    dup_list[index] = malloc(sizeof(char) * 128);
+                    if (!dup_list[index]) {
                         fprintf(stderr, "Memory allocation failed!\n");
                         exit(1);
                     }
-                    strcpy(retain_list[index], array[retain_index - 1]);
-                }
-                
-                // add j-1 to dup_list
-                dup_list[index] = malloc(sizeof(char) * 128);
-                if (!dup_list[index]) {
-                    fprintf(stderr, "Memory allocation failed!\n");
-                    exit(1);
-                }
-                strcpy(dup_list[index], array[j - 1]);
-                
-                index++;
+                    strcpy(dup_list[index], array[retain_index - 1]);
+                    index++;
+                    retain_index = j;
+                } else {
+                    if (!retain_list[index]) {
+                        retain_list[index] = malloc(sizeof(char) * 128);
+                        if (!retain_list[index]) {
+                            fprintf(stderr, "Memory allocation failed!\n");
+                            exit(1);
+                        }
+                        strcpy(retain_list[index], array[retain_index - 1]);
+                    }
 
-                // set the hash to empty string to avoid duplication
+                    dup_list[index] = malloc(sizeof(char) * 128);
+                    if (!dup_list[index]) {
+                        fprintf(stderr, "Memory allocation failed!\n");
+                        exit(1);
+                    }
+                    strcpy(dup_list[index], array[j - 1]);
+                    index++;
+                }
+
                 array[j][0] = '\0';
             }
         }
-
-        // if no match found, retain_index will still be i, so just move on
     }
 
-    // remove duplicate filepath in dup_list get from file_hashes
-    remove_filepath_duplicate(dup_list,retain_list,&index);
-    // files in retain_list might also appear in dup_list,
-    // since one file may have more than 2 duplicates
-    sanitize_dup_retain(dup_list,retain_list,index);
+    remove_filepath_duplicate(dup_list, retain_list, &index);
+    sanitize_dup_retain(dup_list, retain_list, index);
 
     return index;
 }
